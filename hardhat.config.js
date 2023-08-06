dad3b58e3b33e965f165da861c694ca9cd65226f
User <hi@martincebu.la>
2023-08-07 01:35:28 +0200

    Version 0.1

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
 
 
