8b3b1209b218598066ff117a1760a52c03b7dd74
User <hi@martincebu.la>
2023-12-04 16:03:54 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,55 +1,37 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Style from './Ads.module.css';
 
 const Ads = () => {
-    const adScript = {
-        __html: `
-            <script data-cfasync="false" type="text/javascript">
-                if (typeof atAsyncOptions !== 'object') var atAsyncOptions = [];
-                atAsyncOptions.push({
-                    'key': 'f431300ee3c352f99d04fd721b580db9',
-                    'format': 'js',
-                    'async': true,
-                    'container': 'atContainer-f431300ee3c352f99d04fd721b580db9',
-                    'params': {}
-                });
-                var script = document.createElement('script');
-                script.type = "text/javascript";
-                script.async = true;
-                script.src = 'http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js';
-                document.getElementsByTagName('head')[0].appendChild(script);
-            </script>
-            <div id="atContainer-f431300ee3c352f99d04fd721b580db9"></div>
-        `,
-    };
+    useEffect(() => {
+        // Check if the ad script is already defined
+        if (typeof atAsyncOptions === 'undefined') {
+            var atAsyncOptions = [];
+            atAsyncOptions.push({
+                'key': 'f431300ee3c352f99d04fd721b580db9',
+                'format': 'js',
+                'async': true,
+                'container': 'atContainer-f431300ee3c352f99d04fd721b580db9',
+                'params': {}
+            });
+
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.async = true;
+            script.setAttribute('data-cfasync', 'false');
+            script.src = 'http' + (window.location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js';
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
+    }, []);
 
     return (
-        <div className={Style.adbanner_import} dangerouslySetInnerHTML={adScript} />
+        <div className={Style.adbanner_import}>
+            <div id="atContainer-f431300ee3c352f99d04fd721b580db9"></div>
+        </div>
     );
 };
 
 export default Ads;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
