66a720350297f6e8a3b1055af6ff25562b4f2cf3
User <hi@martincebu.la>
2023-12-04 03:01:31 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -6,7 +6,7 @@ const Ads = () => {
     return (
         <div className={Style.adbanner_import}>
             <iframe 
-                src={`${process.env.PUBLIC_URL}/ads.html`} 
+                src="/ads.html" 
                 title="Advertisement"
                 style={{ width: '100%', height: 'auto', border: 'none' }} 
                 scrolling="no">
