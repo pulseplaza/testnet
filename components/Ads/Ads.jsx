d8a6825718d4514437dae7264548730fdd26b8f6
User <hi@martincebu.la>
2023-12-04 02:51:40 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,56 +1,22 @@
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
-  const [adConfig, setAdConfig] = useState({ src: "", width: "", height: "" });
-
-  useEffect(() => {
-    const updateAdConfig = () => {
-      if (window.innerWidth < 551) {
-        setAdConfig({
-          src: "//www.topcreativeformat.com/f43f5857097d74aeb88b88bb4533e37d/invoke.js",
-          width: "300",
-          height: "250"
-        });
-      } else if (window.innerWidth >= 551 && window.innerWidth <= 1024) {
-        setAdConfig({
-          src: "//www.topcreativeformat.com/a8bb164e951cde22acbf0c5047466f7a/invoke.js",
-          width: "468",
-          height: "60"
-        });
-      } else {
-        setAdConfig({
-          src: "//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js",
-          width: "728",
-          height: "90"
-        });
-      }
-    };
-
-    updateAdConfig();
-    window.addEventListener('resize', updateAdConfig);
-
-    return () => {
-      window.removeEventListener('resize', updateAdConfig);
-    };
-  }, []);
-
-  return (
-    <div className={Style.adbanner_import}>
-      <iframe 
-        src={adConfig.src} 
-        width={adConfig.width} 
-        height={adConfig.height} 
-        frameBorder="0" 
-        scrolling="no"
-        title="Ad"
-      ></iframe>
-    </div>
-  );
+    return (
+        <div className={Style.adbanner_import}>
+            <iframe 
+                src={`${process.env.PUBLIC_URL}/ads.html`} 
+                title="Advertisement"
+                style={{ width: '100%', height: 'auto', border: 'none' }} 
+                scrolling="no">
+            </iframe>
+        </div>
+    );
 };
 
 export default Ads;
+
 
 
 
