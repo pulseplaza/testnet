8205d6275db146344e45c3ee3c39e713ad0244a6
User <hi@martincebu.la>
2023-12-04 04:45:04 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,40 +1,29 @@
 
-// import React from "react";
-// import Style from "./Ads.module.css";
-
-// const Ads = () => {
-
-
-//     return (
-//         <div className={Style.adbanner_import}>
-//             <iframe
-//                 data-cfasync="false"
-//                 src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
-//                 width="728"
-//                 height="90"
-//                 frameborder="0"
-//                 scrolling="no"
-//             />
-//         </div>
-//     );
-// };
-
-// export default Ads;
-
-
-
 import React from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
+
+
     return (
         <div className={Style.adbanner_import}>
-            <iframe src="/ads.html" width="728" height="90" frameBorder="0"></iframe>
+            <iframe
+                data-cfasync="false"
+                src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
+                width="728"
+                height="90"
+                frameborder="0"
+                scrolling="no"
+            />
         </div>
     );
 };
 
 export default Ads;
+
+
+
+
 
 
 
