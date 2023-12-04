9bcd287eb666fa32c50b9208d93938bc02fa72d5
User <hi@martincebu.la>
2023-12-04 03:24:52 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -5,12 +5,12 @@ import Style from "./Ads.module.css";
 const Ads = () => {
     return (
         <div className={Style.adbanner_import}>
-            <iframe
+            <script
                 src="/ads.html"
                 title="Advertisement"
                 style={{ width: '100%', height: 'auto', border: 'none' }}
             >
-            </iframe>
+            </script>
         </div>
     );
 };
