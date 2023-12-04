eb5dc561a203f263355068057f3fb92746c490a9
User <hi@martincebu.la>
2023-12-04 03:46:53 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -3,34 +3,35 @@ import React, { useEffect, useState } from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
-    const [adSrc, setAdSrc] = useState("");
+    // const [adSrc, setAdSrc] = useState("");
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 551) {
-                setAdSrc("//www.topcreativeformat.com/f43f5857097d74aeb88b88bb4533e37d/invoke.js");
-            } else if (window.innerWidth >= 551 && window.innerWidth <= 1024) {
-                setAdSrc("//www.topcreativeformat.com/a8bb164e951cde22acbf0c5047466f7a/invoke.js");
-            } else {
-                setAdSrc("//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js");
-            }
-        };
+    // useEffect(() => {
+        // const handleResize = () => {
+        //     if (window.innerWidth < 551) {
+        //         setAdSrc("//www.topcreativeformat.com/f43f5857097d74aeb88b88bb4533e37d/invoke.js");
+        //     } else if (window.innerWidth >= 551 && window.innerWidth <= 1024) {
+        //         setAdSrc("//www.topcreativeformat.com/a8bb164e951cde22acbf0c5047466f7a/invoke.js");
+        //     } else {
+        //         setAdSrc("//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js");
+        //     }
+        // };
 
-        handleResize();
-        window.addEventListener('resize', handleResize);
+    //     handleResize();
+    //     window.addEventListener('resize', handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize);
+    //     };
+    // }, []);
 
     return (
         <div className={Style.adbanner_import}>
             <iframe
                 data-cfasync="false"
-                src={adSrc}
-                title="Ad"
-                style={{ border: 'none', width: '100%', height: 'auto' }}
+                src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
+                width="728"
+                height="90"
+                frameborder="0"
                 scrolling="no"
             />
         </div>
