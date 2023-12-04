88863c56dc05f199a97a87c574e33618736d46c5
User <hi@martincebu.la>
2023-12-04 03:40:26 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,21 +1,45 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
+    const [adSrc, setAdSrc] = useState("");
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 551) {
+                setAdSrc("//www.topcreativeformat.com/f43f5857097d74aeb88b88bb4533e37d/invoke.js");
+            } else if (window.innerWidth >= 551 && window.innerWidth <= 1024) {
+                setAdSrc("//www.topcreativeformat.com/a8bb164e951cde22acbf0c5047466f7a/invoke.js");
+            } else {
+                setAdSrc("//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js");
+            }
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className={Style.adbanner_import}>
             <iframe
-                src="/ads.html"
-                title="Advertisement"
-                style={{ width: '100%', height: 'auto', border: 'none' }}
-            >
-            </iframe>
+                data-cfasync="false"
+                src={adSrc}
+                title="Ad"
+                style={{ border: 'none', width: '100%', height: 'auto' }}
+                scrolling="no"
+            />
         </div>
     );
 };
 
 export default Ads;
+
+
 
 
 
