ccadab2fec44ff49d63e40219e45dc9f06d061e8
User <hi@martincebu.la>
2023-12-04 04:29:34 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -23,38 +23,13 @@
 
 
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
-    const adContainerRef = useRef(null);
-
-    useEffect(() => {
-        // Check if the ad container is available
-        if (!adContainerRef.current) {
-            return;
-        }
-
-        // Define the script content
-        const scriptContent = `
-            atOptions = {
-                'key' : 'f431300ee3c352f99d04fd721b580db9',
-                'format' : 'iframe',
-                'height' : 90,
-                'width' : 728,
-                'params' : {}
-            };
-            document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"></scr' + 'ipt>');
-        `;
-
-        // Set the innerHTML of the container using dangerouslySetInnerHTML
-        adContainerRef.current.innerHTML = `<script data-cfasync="false" type="text/javascript">${scriptContent}</script>`;
-
-    }, []);
-
     return (
-        <div className={Style.adbanner_import} ref={adContainerRef}>
-            {/* Ad will be loaded here */}
+        <div className={Style.adbanner_import}>
+            <iframe src="/ads.html" width="728" height="90" frameBorder="0"></iframe>
         </div>
     );
 };
