75cca9e4a0be5fddcaff7eb3a8c17b2f8960531e
User <hi@martincebu.la>
2023-12-04 13:08:57 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,36 +1,36 @@
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import Style from './Ads.module.css';
 
 const Ads = () => {
-    useEffect(() => {
-        // Check if the script is already loaded
-        if (typeof atAsyncOptions === 'undefined') {
-            var atAsyncOptions = [];
-            atAsyncOptions.push({
-                'key': 'f431300ee3c352f99d04fd721b580db9',
-                'format': 'js',
-                'async': true,
-                'container': 'atContainer-f431300ee3c352f99d04fd721b580db9',
-                'params': {}
-            });
-
-            var script = document.createElement('script');
-            script.type = "text/javascript";
-            script.async = true;
-            script.src = 'http' + (window.location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js';
-            document.getElementsByTagName('head')[0].appendChild(script);
-        }
-    }, []);
+    const adScript = {
+        __html: `
+            <script type="text/javascript">
+                if (typeof atAsyncOptions !== 'object') var atAsyncOptions = [];
+                atAsyncOptions.push({
+                    'key': 'f431300ee3c352f99d04fd721b580db9',
+                    'format': 'js',
+                    'async': true,
+                    'container': 'atContainer-f431300ee3c352f99d04fd721b580db9',
+                    'params': {}
+                });
+                var script = document.createElement('script');
+                script.type = "text/javascript";
+                script.async = true;
+                script.src = 'http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js';
+                document.getElementsByTagName('head')[0].appendChild(script);
+            </script>
+            <div id="atContainer-f431300ee3c352f99d04fd721b580db9"></div>
+        `,
+    };
 
     return (
-        <div className={Style.adbanner_import}>
-            <div id="atContainer-f431300ee3c352f99d04fd721b580db9"></div>
-        </div>
+        <div className={Style.adbanner_import} dangerouslySetInnerHTML={adScript} />
     );
 };
 
 export default Ads;
+
 
 
 
