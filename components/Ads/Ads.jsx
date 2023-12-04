85a2ed9b10a11d5f5d77e11d122adce96116a50c
User <hi@martincebu.la>
2023-12-04 13:03:30 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,25 +1,37 @@
 
-import React from "react";
-import Style from "./Ads.module.css";
+import React, { useEffect } from 'react';
+import Style from './Ads.module.css';
 
 const Ads = () => {
+    useEffect(() => {
+        // Check if the script is already loaded
+        if (typeof atAsyncOptions === 'undefined') {
+            var atAsyncOptions = [];
+            atAsyncOptions.push({
+                'key': 'f431300ee3c352f99d04fd721b580db9',
+                'format': 'js',
+                'async': true,
+                'container': 'atContainer-f431300ee3c352f99d04fd721b580db9',
+                'params': {}
+            });
 
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.async = true;
+            script.src = 'http' + (window.location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js';
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
+    }, []);
 
     return (
         <div className={Style.adbanner_import}>
-            <iframe
-                data-cfasync="false"
-                src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
-                width="728"
-                height="90"
-                frameborder="0"
-                scrolling="no"
-            />
+            <div id="atContainer-f431300ee3c352f99d04fd721b580db9"></div>
         </div>
     );
 };
 
 export default Ads;
+
 
 
 
