247111888a8715c93e1aabd2bd43692dacf471c0
User <hi@martincebu.la>
2023-12-04 04:04:14 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -1,44 +1,73 @@
 
-import React, { useEffect, useState } from "react";
+// import React from "react";
+// import Style from "./Ads.module.css";
+
+// const Ads = () => {
+
+
+//     return (
+//         <div className={Style.adbanner_import}>
+//             <iframe
+//                 data-cfasync="false"
+//                 src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
+//                 width="728"
+//                 height="90"
+//                 frameborder="0"
+//                 scrolling="no"
+//             />
+//         </div>
+//     );
+// };
+
+// export default Ads;
+
+
+
+
+
+
+
+import React, { useEffect } from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
-    // const [adSrc, setAdSrc] = useState("");
+    useEffect(() => {
+        // Create script element
+        const script = document.createElement("script");
+        script.type = "text/javascript";
 
-    // useEffect(() => {
-        // const handleResize = () => {
-        //     if (window.innerWidth < 551) {
-        //         setAdSrc("//www.topcreativeformat.com/f43f5857097d74aeb88b88bb4533e37d/invoke.js");
-        //     } else if (window.innerWidth >= 551 && window.innerWidth <= 1024) {
-        //         setAdSrc("//www.topcreativeformat.com/a8bb164e951cde22acbf0c5047466f7a/invoke.js");
-        //     } else {
-        //         setAdSrc("//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js");
-        //     }
-        // };
+        // Define the script options
+        const atOptions = {
+            'key': 'f431300ee3c352f99d04fd721b580db9',
+            'format': 'iframe',
+            'height': 90,
+            'width': 728,
+            'params': {}
+        };
 
-    //     handleResize();
-    //     window.addEventListener('resize', handleResize);
+        // Set the innerHTML of the script
+        script.innerHTML = `document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"></scr' + 'ipt>');`;
 
-    //     return () => {
-    //         window.removeEventListener('resize', handleResize);
-    //     };
-    // }, []);
+        // Append the script to the document body
+        document.body.appendChild(script);
+
+        // Cleanup the script when the component unmounts
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
 
     return (
         <div className={Style.adbanner_import}>
-            <iframe
-                data-cfasync="false"
-                src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"
-                width="728"
-                height="90"
-                frameborder="0"
-                scrolling="no"
-            />
+
+            document.body.removeChild(script);
+            
         </div>
     );
 };
 
 export default Ads;
+
 
 
 
