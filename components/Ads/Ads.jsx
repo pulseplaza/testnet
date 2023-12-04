09ccb4c84f7e051bd8215ccda07f3408bb82e955
User <hi@martincebu.la>
2023-12-04 04:10:03 +0100

    T Version 1

--- components/Ads/Ads.jsx
+++ components/Ads/Ads.jsx
@@ -27,46 +27,52 @@
 
 
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Style from "./Ads.module.css";
 
 const Ads = () => {
+    const adContainerRef = useRef(null);
+
     useEffect(() => {
-        // Create script element
+        // Check if the ad container is available
+        if (!adContainerRef.current) {
+            return;
+        }
+
+        // Create an iframe element
+        const iframe = document.createElement("iframe");
+        iframe.style.width = "728px";
+        iframe.style.height = "90px";
+        iframe.frameBorder = "0";
+        iframe.scrolling = "no";
+        iframe.marginWidth = "0";
+        iframe.marginHeight = "0";
+
+        // Append the iframe to the container
+        adContainerRef.current.appendChild(iframe);
+
+        // Write the script into the iframe
+        const iframeDoc = iframe.contentWindow.document;
         const script = document.createElement("script");
         script.type = "text/javascript";
+        script.src = "//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js";
+        iframeDoc.body.appendChild(script);
 
-        // Define the script options
-        const atOptions = {
-            'key': 'f431300ee3c352f99d04fd721b580db9',
-            'format': 'iframe',
-            'height': 90,
-            'width': 728,
-            'params': {}
-        };
-
-        // Set the innerHTML of the script
-        script.innerHTML = `document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f431300ee3c352f99d04fd721b580db9/invoke.js"></scr' + 'ipt>');`;
-
-        // Append the script to the document body
-        document.body.appendChild(script);
-
-        // Cleanup the script when the component unmounts
+        // Cleanup
         return () => {
-            document.body.removeChild(script);
-        }
+            adContainerRef.current.removeChild(iframe);
+        };
     }, []);
 
     return (
-        <div className={Style.adbanner_import}>
-
-            document.body.removeChild(script);
-            
+        <div className={Style.adbanner_import} ref={adContainerRef}>
+            {/* Ad will be loaded here */}
         </div>
     );
 };
 
 export default Ads;
+
 
 
 
