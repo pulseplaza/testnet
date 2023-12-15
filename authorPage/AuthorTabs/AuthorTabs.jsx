c8eaf35eb46396fa3ebd06a3779e968aa3c9cbf9
User <hi@martincebu.la>
2023-12-15 09:32:41 +0100

    T Version 7

--- authorPage/AuthorTabs/AuthorTabs.jsx
+++ authorPage/AuthorTabs/AuthorTabs.jsx
@@ -56,68 +56,30 @@ const AuthorTabs = ({
       <div className={Style.AuthorTabs_box}>
         <div className={Style.AuthorTabs_box_left}>
           <div className={Style.AuthorTabs_box_left_btn}>
+
             <button
               className={`${activeBtn == 1 ? Style.active : ""}`}
               onClick={(e) => openTab(e)}
-            >
-              Listed NFTs{""}
-            </button>
+            >My Listed NFTs{""}</button>
 
             <button
               className={`${activeBtn == 2 ? Style.active : ""}`}
               onClick={(e) => openTab(e)}
-            >
-              Owned NFTs{""}
-            </button>
+            >My Owned NFTs{""}</button>
 
             <button
               className={`${activeBtn == 3 ? Style.active : ""}`}
               onClick={(e) => openTab(e)}
-            >
-              My Collections{""}
-            </button>
+            >My Collections{""}</button>
+
           </div>
+
         </div>
 
-
-
-
-
-
-        {/* <div className={Style.AuthorTabs_box_right}>
-          <div
-            className={Style.AuthorTabs_box_right_para}
-            onClick={() => openDropDownList()}
-          >
-            <p>{selectedMenu}</p>
-            {openList ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
-          </div>
-
-          {openList && (
-            <div className={Style.AuthorTabs_box_right_list}>
-              {listArray.map((el, i) => (
-                <div
-                  key={i + 1}
-                  onClick={() => setSelectedMenu(el)}
-                  className={Style.AuthorTabs_box_right_list_item}
-                >
-                  <p>{el}</p>
-                  <span>{selectedMenu == el && <TiTick />}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        </div> */}
-
-
-
-
-
-
-
-
       </div>
+
     </div>
+    
   );
 };
 
