30c8915ec7f8a293d9dd4592932a2494329e8077
User <hi@martincebu.la>
2023-12-19 23:52:27 +0100

    T Version 8

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -5,30 +5,33 @@ import { Readable } from 'stream';
 import { ethers } from 'ethers';
 import { NFTMarketplaceAddress, NFTMarketplaceABI } from '../../Context/constants';
 
+
 export default async function sitemapHandler(req, res) {
   const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL;
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
   const contract = new ethers.Contract(NFTMarketplaceAddress, NFTMarketplaceABI, provider);
 
-  // Fetch market items (NFTs)
-  const marketItems = await contract.fetchMarketItems();
 
   // Fetch collections
   const collections = await contract.getCollections();
+
+  // Fetch market items (NFTs)
+  const marketItems = await contract.fetchMarketItems();
+
 
 
   const baseUrl = process.env.NEXT_PUBLIC_DOMAIN;
 
 
+  // Generate dynamic URLs for collections
+  const collectionUrls = collections.map(collection => {
+    return { url: `${baseUrl}/collection?collectionAddress=${collection.collectionAddress}` };
+  });
+
 
   // Generate dynamic URLs for NFTs
   const nftUrls = marketItems.map(item => {
     return { url: `${baseUrl}/nft?tokenId=${item.tokenId.toString()}` };
-  });
-
-  // Generate dynamic URLs for collections
-  const collectionUrls = collections.map(collection => {
-    return { url: `${baseUrl}/collection?collectionAddress=${collection.collectionAddress}` };
   });
 
 
@@ -49,7 +52,8 @@ export default async function sitemapHandler(req, res) {
   ];
 
 
-  const combinedPaths = [...staticPaths, ...nftUrls, ...collectionUrls];
+  const combinedPaths = [...staticPaths, ...collectionUrls, ...nftUrls];
+
 
   const sitemapStream = new SitemapStream({ hostname: baseUrl });
   const xmlString = await streamToPromise(
