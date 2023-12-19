110795db0ac6a71995d94e314192a6798074de0d
User <hi@martincebu.la>
2023-12-19 22:34:35 +0100

    T Version 8

---
+++ pages/api/sitemap.js
@@ -0,0 +1,62 @@
+
+
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
+import { ethers } from 'ethers';
+import { NFTMarketplaceAddress, NFTMarketplaceABI } from '../../Context/constants';
+
+export default async function sitemapHandler(req, res) {
+  const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL;
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const contract = new ethers.Contract(NFTMarketplaceAddress, NFTMarketplaceABI, provider);
+
+  // Fetch market items (NFTs)
+  const marketItems = await contract.fetchMarketItems();
+
+  // Fetch collections
+  const collections = await contract.getCollections();
+
+
+  const baseUrl = process.env.NEXT_PUBLIC_DOMAIN;
+
+
+
+  // Generate dynamic URLs for NFTs
+  const nftUrls = marketItems.map(item => {
+    return { url: `${baseUrl}/nft?tokenId=${item.tokenId.toString()}` };
+  });
+
+  // Generate dynamic URLs for collections
+  const collectionUrls = collections.map(collection => {
+    return { url: `${baseUrl}/collection?collectionAddress=${collection.collectionAddress}` };
+  });
+
+
+
+  // Static paths
+  const staticPaths = [
+    "/",
+    "/search-nfts",
+    "/search-collections",
+    "/create-nft",
+    "/create-collection",
+    "/coin/tokenomics",
+    "/coin/trade",
+    "/aboutus",
+    "/news",
+    "/fees",
+    "/contact",
+  ];
+
+  
+  const combinedPaths = [...staticPaths, ...nftUrls, ...collectionUrls];
+
+  const sitemapStream = new SitemapStream({ hostname: baseUrl });
+  const xmlString = await streamToPromise(
+    Readable.from(combinedPaths).pipe(sitemapStream)
+  ).then(data => data.toString());
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(xmlString);
+  res.end();
+}
--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,35 +1,79 @@
 
 
-require('dotenv').config(); 
-const fs = require('fs');
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { Readable } = require('stream');
+// require('dotenv').config();
+// const fs = require('fs');
+// const { SitemapStream, streamToPromise } = require('sitemap');
+// const { Readable } = require('stream');
+// const { ethers } = require("ethers");
 
-// List all static paths you want to include in your sitemap
-// For dynamic paths, you might want to fetch them from your server or database
-const paths = [
-  "/",
-  "/search-nfts",
-  "/search-collections",
-  "/create-nft",
-  "/create-collection",
-  "/coin/tokenomics",
-  "/coin/trade",
-  "/aboutus",
-  "/news",
-  "/fees",
-  "/contact",
-];
 
-(async () => {
-  const hostname = process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000';
+// // Import contract address and ABI from your constants
+// const { NFTMarketplaceAddress, NFTMarketplaceABI } = require("../Context/constants");
 
-  const sitemapStream = new SitemapStream({ hostname });
 
-  const xmlString = await streamToPromise(
-    Readable.from(paths).pipe(sitemapStream)
-  ).then((data) => data.toString());
 
-  fs.writeFileSync('public/sitemap.xml', xmlString);
-})();
+// // Static paths
+// const staticPaths = [
+//   "/",
+//   "/search-nfts",
+//   "/search-collections",
+//   "/create-nft",
+//   "/create-collection",
+//   "/coin/tokenomics",
+//   "/coin/trade",
+//   "/aboutus",
+//   "/news",
+//   "/fees",
+//   "/contact",
+// ];
+
+
+
+
+// const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL;
+// console.log("RPC URL:", rpcUrl); // Debug log
+
+
+
+// async function fetchDynamicUrlsFromBlockchain() {
+//   console.log("Fetching dynamic URLs from blockchain...");
+//   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+//   const contract = new ethers.Contract(NFTMarketplaceAddress, NFTMarketplaceABI, provider);
+
+//   const marketItems = await contract.fetchMarketItems();
+//   console.log("Market Items:", marketItems);
+//   let dynamicUrls = [];
+//   const baseUrl = process.env.NEXT_PUBLIC_DOMAIN;
+
+//   marketItems.forEach(item => {
+//     dynamicUrls.push(`${baseUrl}/nft?tokenId=${item.tokenId}`);
+//   });
+
+//   console.log("Dynamic URLs fetched:", dynamicUrls);
+//   return dynamicUrls;
+// }
+
+
+
+// (async () => {
+//   try {
+//     const hostname = process.env.NEXT_PUBLIC_DOMAIN;
+//     console.log("Generating sitemap..."); // Debug log
+
+//     const dynamicUrls = await fetchDynamicUrlsFromBlockchain();
+//     const combinedPaths = [...staticPaths, ...dynamicUrls];
+
+//     const sitemapStream = new SitemapStream({ hostname });
+//     const xmlString = await streamToPromise(Readable.from(combinedPaths).pipe(sitemapStream)).then((data) => data.toString());
+
+//     console.log("Writing sitemap to file..."); // Debug log
+//     fs.writeFileSync('public/sitemap.xml', xmlString);
+//     console.log("Sitemap successfully written to 'public/sitemap.xml'"); // Debug log
+//   } catch (error) {
+//     console.error('Error generating sitemap:', error);
+//   }
+// })();
+
+
+
 
