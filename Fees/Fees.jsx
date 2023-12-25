95251e0b39af3e83b158d2fee4535d736f6471f6
User <hi@martincebu.la>
2023-12-25 02:54:29 +0100

    T Version 8

--- Fees/Fees.jsx
+++ Fees/Fees.jsx
@@ -1,12 +1,10 @@
 
 
 import React from "react";
-// import { TiTick } from "react-icons/ti";
 
 
 //INTERNAL IMPORT
 import Style from "../Fees/Fees.module.css";
-// import { Button } from "../components/componentsindex.js";
 
 
 const Fees = ({ el, i }) => {
@@ -15,29 +13,18 @@ const Fees = ({ el, i }) => {
             <div className={Style.FeesBox_box}>
                 <span className={Style.FeesBox_box_span}>{el.plan}</span>
 
-                {/* <small className={Style.FeesBox_box_small}>
-                    {el.popular || ""}
-                </small>
-                 */}
 
                 <p className={Style.FeesBox_box_price}>{el.price}</p>
 
                 <div className={Style.FeesBox_box_info}>
                     {el.service.map((el, i) => (
                         <p className={Style.FeesBox_box_info_para} key={i + 1}>
-                            {/* <span>
-                                <TiTick />
-                            </span> */}
+                            
                             {el}
                         </p>
                     ))}
                 </div>
 
-                {/* <Button
-                    btnName="Submit"
-                    handleClick={() => { }}
-                    classStyle={Style.button}
-                /> */}
 
             </div>
         </div>
